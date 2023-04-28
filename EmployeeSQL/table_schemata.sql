7e0d29f7d435ed7cecfe5634c232305c44d77a80
sberahim <117812772+sberahim@users.noreply.github.com>
2023-04-28 21:11:40 +1000

    SQL Challenge Files

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -24,16 +24,15 @@ CREATE TABLE departments (
 
 -- Create dept_manager table
 CREATE TABLE dept_manager (
-	dept_no varchar(5) NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
 	emp_no int NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employess(emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Create dept_emp table
 CREATE TABLE dept_emp (
 	emp_no int NOT NULL,
-	dept_no varchar(5) NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
